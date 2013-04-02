9874bfa9fc8afc39d7e04ca6cd96ea4f6384fbbc
steve <sgourley@utah.gov>
2013-04-01 19:34:58 -0600

    amd'd the stuff.

--- maps/README.md
+++
@@ -1,1 +0,0 @@
-Intended to hold the mxd's for any map services that are specific to this app.
--- src/app/App.js
+++ src/app/App.js
@@ -10,27 +10,39 @@ define([
     'ijit/modules/ErrorLogger',
     'ijit/widgets/layout/SideBarToggler',
     'ijit/widgets/layout/PaneStack',
+    'dojo/on',
+    'dojo/_base/array',
+    'dojo/_base/lang',
+    'esri/dijit/editing/Editor',
+    'esri/dijit/AttributeInspector',
+    'esri/layers/FeatureLayer',
+    'esri/config',
+    'esri/tasks/GeometryService',
 
     'dijit/layout/BorderContainer',
-    'dijit/layout/ContentPane'
-], 
+    'dijit/layout/ContentPane'],
 
-function (
-    registry, 
-    dom, 
-    declare, 
-    _WidgetBase, 
-    _TemplatedMixin, 
-    _WidgetsInTemplateMixin, 
-    template, 
-    BaseMap, 
-    ErrorLogger, 
-    SideBarToggler, 
-    PaneStack
-    ) {
-    return declare("app.App", 
-        [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], 
-        {
+    function(
+        registry,
+        dom,
+        declare,
+        _WidgetBase,
+        _TemplatedMixin,
+        _WidgetsInTemplateMixin,
+        template,
+        BaseMap,
+        ErrorLogger,
+        SideBarToggler,
+        PaneStack,
+        on,
+        array,
+        lang,
+        editor,
+        attributeEditor,
+        featureLayer,
+        config,
+        geomService) {
+        return declare("app.App", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         // summary:
         //      The main widget for the app
 
@@ -48,7 +60,7 @@ function (
 
             // esri.config.defaults.io.corsEnabledServers.push("dagrc.utah.gov");
             
-            AGRC.errorLogger = new ErrorLogger({appName: 'ProjectName'});
+            AGRC.errorLogger = new ErrorLogger({appName: 'Broadband.Editing'});
             
             AGRC.app = this;
         },
@@ -56,7 +68,7 @@ function (
             // summary:
             //      Fires when 
             console.log(this.declaredClass + "::" + arguments.callee.nom, arguments);
-        
+
             // set version number
             this.version.innerHTML = AGRC.version;
         },
@@ -88,7 +100,42 @@ function (
             //      Sets up the map
             console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
             
-            this.map = new BaseMap(this.mapDiv, {defaultBaseMap: 'Terrain'});
+            this.map = new BaseMap(this.mapDiv, {defaultBaseMap: 'Lite'});
+
+            on(this.map, "LayersAddResult", lang.hitch(this, 'initEditing'));
+
+            var editLayer = new featureLayer("/arcgis/rest/services/demo/AddressPoints_Kane/FeatureServer/0", {
+                mode: featureLayer.MODE_ONDEMAND
+            });
+
+            console.log(editLayer.mode);
+
+            this.map.addLayers([editLayer]);
+
+            config.defaults.geometryService = new geomService("http://mapserv.utah.gov/ArcGIS/rest/services/Geometry/GeometryServer");
+        },
+        initEditing: function(results) {
+            // sumamry:
+            // initializes the editing settings/widget
+            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
+
+            var featureLayerInfos = array.map(results, function(result) {
+                return {
+                    "featureLayer": result.layer
+                };
+            });
+
+            var settings = {
+                map: this.map,
+                layerInfos: featureLayerInfos
+            };
+
+            var params = {
+                settings: settings
+            };
+
+            var editorWidget = new editor(params, this.editorContainer);
+            editorWidget.startup();
         }
     });
 });
