6f8d2c34119d0b3f12a85a3bb68c5d57d6a90fcc
steve <sgourley@utah.gov>
2013-04-02 18:49:13 -0600

    updating app for editing

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,9 @@
 define([
-    'dijit/registry', 
-    'dojo/dom', 
+    'dijit/registry',
+    'dojo/dom',
     'dojo/_base/declare',
-    'dijit/_WidgetBase', 
-    'dijit/_TemplatedMixin', 
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin',
     'dijit/_WidgetsInTemplateMixin',
     'dojo/text!app/templates/App.html',
     'agrc/widgets/map/BaseMap',
@@ -18,31 +18,37 @@ define([
     'esri/layers/FeatureLayer',
     'esri/config',
     'esri/tasks/GeometryService',
+    'dojo/dom-construct',
+    'dijit/TitlePane',
+    'agrc/widgets/locate/FindGeneric',
 
     'dijit/layout/BorderContainer',
     'dijit/layout/ContentPane'],
 
-    function(
-        registry,
-        dom,
-        declare,
-        _WidgetBase,
-        _TemplatedMixin,
-        _WidgetsInTemplateMixin,
-        template,
-        BaseMap,
-        ErrorLogger,
-        SideBarToggler,
-        PaneStack,
-        on,
-        array,
-        lang,
-        editor,
-        attributeEditor,
-        featureLayer,
-        config,
-        geomService) {
-        return declare("app.App", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+function(
+registry,
+dom,
+declare,
+_WidgetBase,
+_TemplatedMixin,
+_WidgetsInTemplateMixin,
+template,
+BaseMap,
+ErrorLogger,
+SideBarToggler,
+PaneStack,
+on,
+array,
+lang,
+editor,
+attributeEditor,
+featureLayer,
+config,
+geomService,
+construct,
+titlePane,
+findGeneric) {
+    return declare("app.App", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
         // summary:
         //      The main widget for the app
 
@@ -52,19 +58,21 @@ define([
 
         // map: agrc.widgets.map.Basemap
         map: null,
-        
-        constructor: function(){
+
+        constructor: function() {
             // summary:
             //      first function to fire after page loads
             console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
 
             // esri.config.defaults.io.corsEnabledServers.push("dagrc.utah.gov");
-            
-            AGRC.errorLogger = new ErrorLogger({appName: 'Broadband.Editing'});
-            
+
+            AGRC.errorLogger = new ErrorLogger({
+                appName: 'Broadband.Editing'
+            });
+
             AGRC.app = this;
         },
-        postCreate: function () {
+        postCreate: function() {
             // summary:
             //      Fires when 
             console.log(this.declaredClass + "::" + arguments.callee.nom, arguments);
@@ -72,7 +80,7 @@ define([
             // set version number
             this.version.innerHTML = AGRC.version;
         },
-        startup: function () {
+        startup: function() {
             // summary:
             //      Fires after postCreate when all of the child widgets are finished laying out.
             console.log(this.declaredClass + "::" + arguments.callee.nom, arguments);
@@ -80,46 +88,88 @@ define([
             // call this before creating the map to make sure that the map container is 
             // the correct size
             this.inherited(arguments);
-            
-            var ps;
-            var sb;
 
-            ps = new PaneStack(null, this.paneStack);
-            
+            this.paneStack = new PaneStack(null, this.paneStackContainer);
+
             this.initMap();
-            
-            sb = new SideBarToggler({
+
+            new SideBarToggler({
                 sidebar: this.sideBar.domNode,
                 mainContainer: this.mainContainer,
                 map: this.map,
                 centerContainer: this.centerContainer.domNode
             }, this.sidebarToggle);
         },
-        initMap: function(){
+        initMap: function() {
             // summary:
             //      Sets up the map
             console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
-            
-            this.map = new BaseMap(this.mapDiv, {defaultBaseMap: 'Lite'});
 
-            on(this.map, "LayersAddResult", lang.hitch(this, 'initEditing'));
-
-            var editLayer = new featureLayer("/arcgis/rest/services/demo/AddressPoints_Kane/FeatureServer/0", {
-                mode: featureLayer.MODE_ONDEMAND
+            this.map = new BaseMap(this.mapDiv, {
+                defaultBaseMap: 'Lite'
             });
 
-            console.log(editLayer.mode);
-
-            this.map.addLayers([editLayer]);
+            on(this.map, 'Load', lang.hitch(this, 'mapLoad'));
+            on(this.map, "LayersAddResult", lang.hitch(this, 'initEditing'));
 
             config.defaults.geometryService = new geomService("http://mapserv.utah.gov/ArcGIS/rest/services/Geometry/GeometryServer");
         },
-        initEditing: function(results) {
-            // sumamry:
-            // initializes the editing settings/widget
+        mapLoad: function() {
             console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
 
-            var featureLayerInfos = array.map(results, function(result) {
+            this.determineCounty();
+        },
+        determineCounty: function() {
+            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
+
+            var finder = new findGeneric({
+                map: this.map,
+                layerName: 'SGID10.BOUNDARIES.Counties',
+                searchFieldName: 'NAME',
+                label: 'County',
+                fieldLabel: 'Name'
+            });
+
+            this.countypane = new titlePane({
+                title: "Choose a County",
+                content: finder,
+                open: true
+            });
+
+            this.paneStack.add(this.countypane);
+
+            on(finder, 'Find', lang.hitch(this, 'addFeatureLayer'));
+        },
+        addFeatureLayer: function(find) {
+            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
+
+            var lookup = {
+                'kane': 0
+            };
+
+            var key = find.query.toLowerCase();
+            var id = lookup[key];
+
+            if (id < 0) {
+                return;
+            }
+
+            if (this.editLayer) this.map.removeLayer(this.editLayer);
+
+            this.editLayer = new featureLayer("/arcgis/rest/services/demo/AddressPoints_Kane/FeatureServer/" + id, {
+                mode: featureLayer.MODE_ONDEMAND
+            });
+
+            this.map.addLayers([this.editLayer]);
+
+            return true;
+        },
+        initEditing: function(layer) {
+            // sumamry:
+            //      initializes the editing settings/widget
+            console.info(this.declaredClass + "::" + arguments.callee.nom, arguments);
+
+            var featureLayerInfos = array.map(layer, function(result) {
                 return {
                     "featureLayer": result.layer
                 };
@@ -134,7 +184,20 @@ define([
                 settings: settings
             };
 
-            var editorWidget = new editor(params, this.editorContainer);
+            var editorWidget = new editor(params);
+
+            if (this.editingpane) 
+                this.paneStack.remove(this.editingpane);
+
+            this.editingpane = new titlePane({
+                title: "Edit Address Points",
+                content: editorWidget,
+                open: true
+            });
+
+            this.countypane.toggle();
+            this.paneStack.add(this.editingpane);
+
             editorWidget.startup();
         }
     });
