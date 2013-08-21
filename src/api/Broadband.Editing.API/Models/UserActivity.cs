5835c7bcb648202feb88890865c1e5f287111c58
steve <sgourley@utah.gov>
2013-08-21 14:31:30 -0600

    date to ticks

--- src/api/Broadband.Editing.API/Models/UserActivity.cs
+++ src/api/Broadband.Editing.API/Models/UserActivity.cs
@@ -5,11 +5,12 @@ namespace Broadband.Editing.API.Models
     public class UserActivity
     {
         public string UserName { get; set; }
+
         public string[] Changes { get; set; }
 
-        public DateTime Date
+        public long Date
         {
-            get { return DateTime.UtcNow; }
+            get { return DateTime.UtcNow.Ticks; }
         }
     }
 }
