0a2a5f8619de52116fa5beec550adc3c21084745
steve <sgourley@utah.gov>
2013-08-21 14:48:31 -0600

    renaming to app

--- src/api/Broadband.Editing.API/Global.asax
+++ src/api/Broadband.Editing.API/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Broadband.Editing.API.WebApiApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Broadband.Editing.API.App" Language="C#" %>
--- src/api/Broadband.Editing.API/Global.asax.cs
+++ src/api/Broadband.Editing.API/Global.asax.cs
@@ -1,12 +1,12 @@
 ﻿using System.Web.Http;
+using Ninject;
 
 namespace Broadband.Editing.API
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
-    public class WebApiApplication : System.Web.HttpApplication
+    public class App : System.Web.HttpApplication
     {
+        public static IKernel Kernel { get; set; }
+
         protected void Application_Start()
         {
             WebApiConfig.Register(GlobalConfiguration.Configuration);
