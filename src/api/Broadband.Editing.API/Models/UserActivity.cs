43bc4b0d5b9c0592250d586ecb15eac4f083c32f
steve <sgourley@utah.gov>
2013-08-21 14:12:15 -0600

    db activity model

---
+++ src/api/Broadband.Editing.API/Models/UserActivity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Broadband.Editing.API.Models
+{
+    public class UserActivity
+    {
+        public string UserName { get; set; }
+        public string[] Changes { get; set; }
+
+        public DateTime Date
+        {
+            get { return DateTime.UtcNow; }
+        }
+    }
+}
