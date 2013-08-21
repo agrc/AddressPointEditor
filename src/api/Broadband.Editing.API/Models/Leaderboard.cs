3996cf884bd6f1078eb8f94d862af518b015214a
steve <sgourley@utah.gov>
2013-08-21 16:09:01 -0600

    Leaderboard endpoint addition

---
+++ src/api/Broadband.Editing.API/Models/Leaderboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Broadband.Editing.API.Database.Indexes;
+
+namespace Broadband.Editing.API.Models
+{
+    public class Leaderboard
+    {
+        public Leaderboard(LeaderItem currentUser, IList<LeaderboardIndex.ReduceResult> standings)
+        {
+            Current = currentUser;
+
+        }
+
+        public LeaderItem Current { get; set; }
+        public IList<LeaderItem> Standings { get; set; }
+    }
+
+    public class LeaderItem
+    {
+        public string Name { get; set; }
+        public int EditCount { get; set; }
+        public string RankCss { get; set; }
+    }
+}
