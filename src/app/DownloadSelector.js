711f749d25ff04da29191bec1febdf1b1f4e0ded
steve <sgourley@utah.gov>
2013-08-26 17:39:24 -0600

    download selector is working.
    moved events to template. posting to url.

--- src/app/DownloadSelector.js
+++ src/app/DownloadSelector.js
@@ -2,53 +2,56 @@ define([
         'dojo/_base/declare',
         'dojo/_base/lang',
         'dojo/_base/array',
+        'dojo/_base/event',
 
         'dojo/dom',
         'dojo/on',
         'dojo/aspect',
         'dojo/dom-class',
         'dojo/dom-construct',
+        'dojo/dom-attr',
+        'dojo/Stateful',
 
         'dijit/_WidgetBase',
         'dijit/_TemplatedMixin',
         'dijit/layout/StackContainer',
         'dijit/layout/ContentPane',
+        'dijit/_WidgetsInTemplateMixin',
 
         'dojo/text!app/templates/DownloadSelector.html',
-        'dojo/text!app/templates/DownloadCoordinateSystem.html',
-        'dojo/text!app/templates/DownloadFileType.html',
-        'dojo/text!app/templates/DownloadCounty.html',
-
-        'mustache/mustache'
+        'agrc/modules/JSONLoader!app/data/counties.json'
     ],
 
     function(
         declare,
         lang,
         array,
+        event,
 
         dom,
         on,
         aspect,
         domClass,
         domConstruct,
+        domAttr,
+        Stateful,
 
         _WidgetBase,
         _TemplatedMixin,
         StackContainer,
         ContentPane,
+        _WidgetsInTemplateMixin,
 
         template,
-        page1,
-        page2,
-        page3,
 
-        mustache
+        counties
     ) {
         // summary:
         //      Handles retrieving and displaying the data in the popup.
-        return declare("app.downloadSelector", [_WidgetBase, _TemplatedMixin], {
+        return declare("app.downloadSelector", [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
             baseClass: 'download-selector',
+
+            widgetsInTemplate: true,
 
             templateString: template,
 
@@ -57,71 +60,62 @@ define([
 
             download: null,
 
+            _setPostUrlAttr: {
+                node: 'downloadForm',
+                type: 'attribute',
+                attribute: 'action'
+            },
+            _setSystemAttr: {
+                node: 'systemHidden',
+                type: 'attribute',
+                attribute: 'value'
+            },
+            _setTypeAttr: {
+                node: 'typeHidden',
+                type: 'attribute',
+                attribute: 'value'
+            },
+            _setCountyAttr: {
+                node: 'countyHidden',
+                type: 'attribute',
+                attribute: 'value'
+            },
             constructor: function() {
-                console.log(this.declaredClass + "::constructor", arguments);
+                console.info(this.declaredClass + "::constructor", arguments);
             },
             postCreate: function() {
                 // summary:
                 //      dom is ready
-                console.log(this.declaredClass + "::postCreate", arguments);
+                console.info(this.declaredClass + "::postCreate", arguments);
 
                 this.inherited(arguments);
 
-                this.download = {};
+                this.download = new Stateful({
+                    system: null,
+                    type: null,
+                    county: null
+                });
 
-                this.coordinateTemplate = domConstruct.toDom(mustache.compile(page1)({
-                    id: this.id
-                }));
-                this.fileTypeTemplate = domConstruct.toDom(mustache.compile(page2)({
-                    id: this.id
-                }));
-                this.countyTemplate = domConstruct.toDom(mustache.compile(page3)({
-                    id: this.id,
-                    county: ['Salt Lake', 'Provo']
-                }));
+                this.download.watch("system", lang.hitch(this, 'updateHiddenValues'));
+                this.download.watch("type", lang.hitch(this, 'updateHiddenValues'));
+                this.download.watch("county", lang.hitch(this, 'updateHiddenValues'));
 
-                this.sc = new StackContainer({}, this.wizardNode);
-                var cp1 = new ContentPane({
-                    content: this.coordinateTemplate
-                }),
-                    cp2 = new ContentPane({
-                        content: this.fileTypeTemplate
-                    }),
-                    cp3 = new ContentPane({
-                        content: this.countyTemplate
-                    });
+                this.pages = [this.cp1, this.cp2, this.cp3];
 
-                this.pages = [cp1, cp2, cp3];
-
-                this.sc.addChild(cp1);
-                this.sc.addChild(cp2);
-                this.sc.addChild(cp3);
+                this.hydrateCountySelect();
 
                 this.sc.startup();
-
-                this.wireEvents();
             },
-            wireEvents: function() {
-                console.log(this.declaredClass + "::wireEvents", arguments);
-
-                this.own(
-                    on(dom.byId(this.id + '_stateplain_north'), 'click', lang.hitch(this, lang.partial(this.setDownloadFilter, 'system', 'state_plain_north'))),
-                    on(dom.byId(this.id + '_stateplain_central'), 'click', lang.hitch(this, lang.partial(this.setDownloadFilter, 'system', 'state_plain_central'))),
-                    on(dom.byId(this.id + '_stateplain_south'), 'click', lang.hitch(this, lang.partial(this.setDownloadFilter, 'system', 'state_plain_south'))),
-                    on(dom.byId(this.id + '_utm_zone12n'), 'click', lang.hitch(this, lang.partial(this.setDownloadFilter, 'system', 'utm12')))
-                );
-
-                this.own(
-                    on(dom.byId(this.id + '_shapefile'), 'click', lang.hitch(this, lang.partial(this.setFileType, 'type', 'shapefile'))),
-                    on(dom.byId(this.id + '_fgdb'), 'click', lang.hitch(this, lang.partial(this.setDownloadFilter, 'type', 'fgdb')))
-                );
-
-                this.own(
-                    on(dom.byId(this.id + '_county'), 'change', lang.hitch(this, lang.partial(this.setDownloadFilter, 'county')))
-                );
+            updateHiddenValues: function (prop, oldValue, newValue) {
+                // summary:
+                //      updates the hidden inputs for the form post
+                // param: type or return: type
+                console.log(this.declaredClass + "::updateHiddenValues", arguments);
+             
+                this.set(prop, newValue);
             },
             showNextPage: function() {
-                console.log(this.declaredClass + "::showNextPage", arguments);
+                console.info(this.declaredClass + "::showNextPage", arguments);
 
                 if (this.currentPage < 0) {
                     domClass.add(this.backButton, 'hidden');
@@ -139,7 +133,7 @@ define([
                 this.sc.selectChild(this.pages[this.currentPage]);
             },
             back: function() {
-                console.log(this.declaredClass + "::back", arguments);
+                console.info(this.declaredClass + "::back", arguments);
 
                 if (this.currentPage > 0) {
                     this.currentPage--;
@@ -151,34 +145,85 @@ define([
 
                 this.sc.selectChild(this.pages[this.currentPage]);
             },
-            setDownloadFilter: function(prop, value) {
-                console.log(this.declaredClass + "::setDownloadFilter", arguments);
+            setDownloadFilter: function(evt) {
+                console.info(this.declaredClass + "::setDownloadFilter", arguments);
 
-                if(value && value.target)
-                {
-                    value = value.target.value;
+                var node = evt.target,
+                    prop = node.getAttribute("data-prop"),
+                    value = null;
+
+                value = node.getAttribute("data-" + prop);
+
+                if (prop === 'county') {
+                    value = node.value;
                 }
 
-                this.download[prop] = value;
+                this.download.set(prop, value);
                 this.showNextPage();
 
                 this.showDownloadButton();
             },
-            showDownloadButton: function(){
-                console.log(this.declaredClass + "::showDownloadButton", arguments);
+            showDownloadButton: function() {
+                console.info(this.declaredClass + "::showDownloadButton", arguments);
 
-                var props = ['system', 'type', 'county'];
-                if(array.every(props, function(item){
-                    return item;
-                },this))
-                {
-                    console.log('show download button');
+                if (!this.valid()) {
+                    domClass.add(this.submitButton, 'hidden');
+                    domAttr.set(this.submitButton, 'disabled', true);
+
+                    return;
+                }
+
+                domClass.remove(this.submitButton, 'hidden');
+                domAttr.set(this.submitButton, 'disabled', false);
+
+                this.set('postUrl', '/download');
+            },
+            hydrateCountySelect: function() {
+                console.log(this.declaredClass + "::hydrateCountySelect", arguments);
+
+                var countyNames = counties.result.sort(
+                    function(a, b) {
+                        a = a.attributes.name;
+                        b = b.attributes.name;
+
+                        if (a > b)
+                            return 1;
+                        if (a < b)
+                            return -1;
+
+                        return 0;
+                    });
+
+                array.forEach(countyNames,
+                    function(v) {
+                        domConstruct.create('option', {
+                            value: v.attributes.name,
+                            innerHTML: v.attributes.name.replace(/\w\S*/g, function(txt) {
+                                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                            })
+                        }, this.countySelect);
+                    }, this);
+            },
+            validate: function(evt) {
+                console.info(this.declaredClass + "::validate", arguments);
+
+                if (!this.valid()) {
+                    domClass.add(this.submitButton, 'hidden');
+                    domAttr.set(this.submitButton, 'disabled', true);
+
+                    event.stop(evt);
+                    return;
                 }
             },
-            requestDownload: function() {
-                console.log(this.declaredClass + "::requestDownload", arguments);
+            valid: function() {
+                // summary:
+                //      validates the download object
+                console.log(this.declaredClass + "::valid", arguments);
 
+                var props = ['system', 'type', 'county'];
+                return array.every(props, function(item) {
+                    return !!this.download.get(item);
+                }, this);
             }
-
         });
     });
