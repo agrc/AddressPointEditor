b052c72fed64c038be6d67de9c05263c000dbe98
steveoh <sgourley@utah.gov>
2013-10-08 17:31:48 -0600

    fixes #17 with a test

--- src/app/tests/spec/SpecEditor.js
+++ src/app/tests/spec/SpecEditor.js
@@ -1,112 +1,142 @@
 define([
-    'intern!object',
+        'intern!object',
 
-    'intern/chai!assert',
-    
-    'dojo/_base/window',
+        'intern/chai!assert',
 
-    'dojo/dom-construct',
-    'dojo/dom-class',
+        'dojo/_base/window',
 
-    'app/Editor'
+        'dojo/dom-construct',
+        'dojo/dom-class',
 
-],
+        'app/Editor'
 
-function (
-    registerSuite,
-    assert,
-    win,
-    domConstruct,
-    domClass,
-    Editor
+    ],
+
+    function(
+        registerSuite,
+        assert,
+        win,
+        domConstruct,
+        domClass,
+        Editor
     ) {
-    var testWidget;
-    var mockMap = {
-        loaded: true
-    };
-
-    registerSuite({
-        name: 'app/Editor',
-
-        beforeEach: function () {
-            testWidget = new Editor({
-                map: mockMap
-            }, domConstruct.create('div', {}, win.body()));
-        },
-        afterEach: function () {
-            testWidget.destroy();
-            testWidget = null;
-        },
-
-        'creates_a_valid_object': function () {
-            assert.equal(testWidget.declaredClass, 'app.editor');
-        },
-
-        'addUndoState': {
-            'enables undo button': function () {
-                testWidget.addUndoState([1,2,3]);
-
-                assert.isFalse(domClass.contains(testWidget.undoNode, 'disabled'));
+        var testWidget;
+        var mockMap = {
+            loaded: true
+        };
+        var mockFeaturelayer = {
+            applyEdits: function(){
+                return true;
             }
-        },
-        'undo': {
-            'disables undo button if there are no more operations to undo': function () {
-                domClass.remove(testWidget.undoNode, 'disabled');
-                testWidget.undoManager.canUndo = true;
+        };
+        var mockGraphic = {
+            setGeometry: function()
+            {
+                return true;
+            },
+            setAttributes: function(){
+                return true;
+            },
+            geometry: {
+                toJson: function() {
+                    return "";
+                }
+            }
+        };
+
+        registerSuite({
+            name: 'app/Editor',
+
+            beforeEach: function() {
+                testWidget = new Editor({
+                    map: mockMap,
+                    editLayer: mockFeaturelayer
+                }, domConstruct.create('div', {}, win.body()));
+            },
+            afterEach: function() {
+                testWidget.destroy();
+                testWidget = null;
+            },
+
+            'creates_a_valid_object': function() {
+                assert.equal(testWidget.declaredClass, 'app.editor');
+            },
+
+            'bug_17': function() {
+                //operationType, graphic, original
+                testWidget.addUndoState("update", mockGraphic, mockGraphic);
 
                 testWidget.undo();
-
-                assert.isFalse(domClass.contains(testWidget.undoNode, 'disabled'));
-
-                testWidget.undoManager.canUndo = false;
                 testWidget.undo();
 
-                assert.isTrue(domClass.contains(testWidget.undoNode, 'disabled'));
+                assert.isTrue(domClass.contains(testWidget.undoNode, 'disabled'), "expecting disabled to still be there and it's not");
             },
-            'enables redo button': function () {
-                domClass.add(testWidget.undoNode, 'disabled');
 
-                testWidget.undo();
+            'addUndoState': {
+                'enables undo button': function() {
+                    testWidget.addUndoState("update", mockGraphic, mockGraphic);
 
-                assert.isFalse(domClass.contains(testWidget.redoNode, 'disabled'));
+                    assert.isFalse(domClass.contains(testWidget.undoNode, 'disabled'));
+                }
+            },
+            'undo': {
+                'disables undo button if there are no more operations to undo': function() {
+                    domClass.remove(testWidget.undoNode, 'disabled');
+                    testWidget.undoManager.canUndo = true;
+
+                    testWidget.undo();
+
+                    assert.isTrue(domClass.contains(testWidget.undoNode, 'disabled'));
+
+                    testWidget.undoManager.canUndo = false;
+                    testWidget.undo();
+
+                    assert.isTrue(domClass.contains(testWidget.undoNode, 'disabled'));
+                },
+                'enables redo button': function() {
+                    domClass.add(testWidget.undoNode, 'disabled');
+
+                    testWidget.undo();
+
+                    assert.isFalse(domClass.contains(testWidget.redoNode, 'disabled'));
+                }
+            },
+            'updateUndoRedoCounts': {
+                'shows correct counts when there is no available undos': function() {
+                    testWidget.undoManager.length = 0;
+
+                    testWidget.updateUndoRedoCounts();
+
+                    assert.strictEqual(testWidget.undoCount, '');
+                    assert.strictEqual(testWidget.redoCount, '');
+                },
+                'shows the correct counts when there are 3 undos but none have been undone': function() {
+                    testWidget.undoManager.length = 3;
+                    testWidget.undoManager.position = 3;
+
+                    testWidget.updateUndoRedoCounts();
+
+                    assert.strictEqual(testWidget.undoCount, 3);
+                    assert.strictEqual(testWidget.redoCount, '');
+                },
+                'shows the correct counts when there are 3 undos and 2 have been undone': function() {
+                    testWidget.undoManager.length = 3;
+                    testWidget.undoManager.position = 1;
+
+                    testWidget.updateUndoRedoCounts();
+
+                    assert.strictEqual(testWidget.undoCount, 1);
+                    assert.strictEqual(testWidget.redoCount, 2);
+                },
+                'shows the correct counts when there are 3 undos and 3 have been undone': function() {
+                    testWidget.undoManager.length = 3;
+                    testWidget.undoManager.position = 0;
+
+                    testWidget.updateUndoRedoCounts();
+
+                    assert.strictEqual(testWidget.undoCount, '');
+                    assert.strictEqual(testWidget.redoCount, 3);
+                }
             }
-        },
-        'updateUndoRedoCounts': {
-            'shows correct counts when there is no available undos': function () {
-                testWidget.undoManager.length = 0;
-
-                testWidget.updateUndoRedoCounts();
-
-                assert.strictEqual(testWidget.undoCount, '');
-                assert.strictEqual(testWidget.redoCount, '');
-            },
-            'shows the correct counts when there are 3 undos but none have been undone': function () {
-                testWidget.undoManager.length = 3;
-                testWidget.undoManager.position = 3;
-
-                testWidget.updateUndoRedoCounts();
-
-                assert.strictEqual(testWidget.undoCount, 3);
-                assert.strictEqual(testWidget.redoCount, '');
-            },
-            'shows the correct counts when there are 3 undos and 2 have been undone': function () {
-                testWidget.undoManager.length = 3;
-                testWidget.undoManager.position = 1;
-
-                testWidget.updateUndoRedoCounts();
-
-                assert.strictEqual(testWidget.undoCount, 1);
-                assert.strictEqual(testWidget.redoCount, 2);
-            },
-            'shows the correct counts when there are 3 undos and 3 have been undone': function () {
-                testWidget.undoManager.length = 3;
-                testWidget.undoManager.position = 0;
-
-                testWidget.updateUndoRedoCounts();
-
-                assert.strictEqual(testWidget.undoCount, '');
-                assert.strictEqual(testWidget.redoCount, 3);
-            }
-        }
+        });
     });
-});
