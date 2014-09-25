2526623524997c124d13d76e28dc593b4ba636db
steveoh <sgourley@utah.gov>
2014-09-25 15:07:21 -0600

    Release v1.2.1

--- src/app/config.js
+++ src/app/config.js
@@ -26,7 +26,7 @@ define([
 
         // version.: String
         //      The version number.
-        version: '1.2.0',
+        version: '1.2.1',
 
         //apiKey: 'AGRC-B5D62BD2151902', // localhost
         apiKey: apiKey,
