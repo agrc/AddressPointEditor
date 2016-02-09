b2b71fe8af27b4713168623634ad89e64c4ae07d
steveoh <sgourley@utah.gov>
2016-02-08 19:08:27 -0700

    remove used api

--- src/api/Broadband.Editing.API/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Broadband.Editing.API.App" Language="C#" %>
--- src/api/Broadband.Editing.API/Models/ActivityResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace Broadband.Editing.API.Models
-{
-    public class ActivityResponse
-    {
-        public string UserName { get; set; }
-    }
-}
