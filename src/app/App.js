580367e8d9798d1bd532720394171a1591b6e014
steveoh <sgourley@utah.gov>
2018-08-09 10:01:29 -0600

    add zach and greg
    closes #95

--- src/app/App.js
+++ src/app/App.js
@@ -181,7 +181,7 @@ define([
             this.changeRequest = new ChangeRequest({
                 map: this.map,
                 redliner: config.urls.redline,
-                toIds: [3, 5, 6]
+                toIds: [3, 5, 6, 7, 12]
             }, this.suggestChangeDiv),
             this.downloadWizard = new DownloadSelector({},
                 this.downloadDiv),
